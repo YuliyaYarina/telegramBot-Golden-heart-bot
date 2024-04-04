2d8f240e08296c9de5860289856308c189bd1b25
Syrgak <Kary6ekov.s@gmail.com>
2024-04-04 20:31:07 +0200

    the project skeleton has been created

---
+++ src/main/java/com/example/golden/heart/bot/GoldenHeartBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.golden.heart.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenHeartBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenHeartBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/golden/heart/bot/GoldenHeartBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.golden.heart.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenHeartBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
