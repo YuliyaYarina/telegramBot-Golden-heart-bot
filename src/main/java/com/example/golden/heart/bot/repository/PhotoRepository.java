68d7d5a8053fab2e3e2124193c20adce63b2cd64
Syrgak <Kary6ekov.s@gmail.com>
2024-04-15 20:02:44 +0200

    model Photo was created

---
+++ src/main/java/com/example/golden/heart/bot/repository/PhotoRepository.java
@@ -0,0 +1,8 @@
+package com.example.golden.heart.bot.repository;
+
+import com.example.golden.heart.bot.model.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PhotoRepository extends JpaRepository<Photo, Long> {
+
+}
