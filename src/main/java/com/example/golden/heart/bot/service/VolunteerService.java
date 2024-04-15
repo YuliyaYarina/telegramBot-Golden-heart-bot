f2c1bc9acb76cd590006a24147ef013156f8e556
Syrgak <Kary6ekov.s@gmail.com>
2024-04-15 19:55:51 +0200

    service and controller for Volunteer was created

---
+++ src/main/java/com/example/golden/heart/bot/service/VolunteerService.java
@@ -0,0 +1,26 @@
+package com.example.golden.heart.bot.service;
+
+import com.example.golden.heart.bot.model.Volunteer;
+import com.example.golden.heart.bot.repository.VolunteerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class VolunteerService {
+    @Autowired
+    private VolunteerRepository volunteerRepository;
+
+    public Volunteer saveVolunteer(Volunteer volunteer) {
+        return volunteerRepository.save(volunteer);
+    }
+
+    public Volunteer editeVolunteer(Volunteer volunteer) {
+        return volunteerRepository.save(volunteer);
+    }
+
+    public Volunteer getVolunteer(Long id) {
+        return volunteerRepository.findById(id).get();
+    }
+
+    public void removeVolunteer(Long id) {
+        volunteerRepository.deleteById(id);
+    }
+}
