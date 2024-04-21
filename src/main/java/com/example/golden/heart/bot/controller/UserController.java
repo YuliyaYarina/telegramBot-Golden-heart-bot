b74a3e6a76e8856a99ab7dfc121691b18cafa826
Syrgak <Kary6ekov.s@gmail.com>
2024-04-21 19:29:54 +0200

    created service and controller for User

---
+++ src/main/java/com/example/golden/heart/bot/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.example.golden.heart.bot.controller;
+
+import com.example.golden.heart.bot.model.User;
+import com.example.golden.heart.bot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public User saveUser(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @PutMapping
+    public ResponseEntity<User> editeUser(@RequestBody User user) {
+        User foundUser = userService.edite(user);
+        if (foundUser == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        User user = userService.getById(id);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(userService.getById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> removeUser (@PathVariable Long id) {
+        User user = userService.getById(id);
+        if (user != null) {
+            userService.removeById(id);
+            return ResponseEntity.ok(user);
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
