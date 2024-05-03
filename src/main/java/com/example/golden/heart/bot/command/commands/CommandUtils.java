fbedce0524d09af1d652ef9954fa7b8682b56dfc
Syrgak <Kary6ekov.s@gmail.com>
2024-05-03 19:27:53 +0200

    написал статические методы для пойска имени и userName пользователя

--- src/main/java/com/example/golden/heart/bot/command/commands/CommandUtils.java
+++ src/main/java/com/example/golden/heart/bot/command/commands/CommandUtils.java
@@ -1,5 +1,6 @@
 package com.example.golden.heart.bot.command.commands;
 
+import com.example.golden.heart.bot.exceptions.UnknownUpdateException;
 import com.pengrad.telegrambot.model.Update;
 
 /**
@@ -12,10 +13,32 @@ public class CommandUtils {
      * @return возвращает id текущего чата
      */
     public static Long getChatId(Update update) {
-        if (update.message()!=  null){
+        if (update.message() !=  null){
         return update.message().chat().id();
-    } else {
+        } else if (update.callbackQuery() != null) {
             return update.callbackQuery().message().chat().id();
+        } else {
+            throw new UnknownUpdateException("Не известная update");
         }
 }
+
+    public static String getFirstName(Update update) {
+        if (update.message() != null) {
+            return update.message().from().firstName();
+        } else if (update.callbackQuery() != null) {
+            return update.callbackQuery().from().firstName();
+        } else {
+            throw new UnknownUpdateException();
+        }
+    }
+
+    public static String getUserName(Update update) {
+        if (update.message() != null) {
+            return update.message().from().username();
+        } else if (update.callbackQuery() != null) {
+            return update.callbackQuery().from().username();
+        } else {
+            throw new UnknownUpdateException();
+        }
+    }
 }
