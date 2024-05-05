5a9b35ea573e9f4a4ade0896d10d2f953fdc02c4
Syrgak <Kary6ekov.s@gmail.com>
2024-05-05 11:51:34 +0200

    button call Volunteer were implemented

--- src/main/java/com/example/golden/heart/bot/command/commands/start/info/VolonterCommand.java
+++ src/main/java/com/example/golden/heart/bot/command/commands/start/info/VolonterCommand.java
@@ -1,31 +1,52 @@
 package com.example.golden.heart.bot.command.commands.start.info;
 
+import com.example.golden.heart.bot.model.User;
 import com.example.golden.heart.bot.service.TelegramBotSender;
+import com.example.golden.heart.bot.service.UserService;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 
 import java.util.HashMap;
 import java.util.Map;
 
+
 import static com.example.golden.heart.bot.command.commands.CommandUtils.getChatId;
+import static com.example.golden.heart.bot.model.Role.VOLUNTEER;
 
 public class VolonterCommand implements com.example.golden.heart.bot.command.Command {
+    String message;
     private TelegramBotSender telegramBotSender;
 
-    public VolonterCommand(TelegramBotSender telegramBotSender) {
+    private UserService userService;
+
+    public VolonterCommand(TelegramBotSender telegramBotSender, UserService userService) {
+        this.userService = userService;
         this.telegramBotSender = telegramBotSender;
     }
 
     @Override
     public void execute(Update update) {
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        markup.addRow(new InlineKeyboardButton("Назад").callbackData("/catAndDog"));
 
-        Map<String,String> map = new HashMap<>();
-        map.put("Назад", "/catAndDog");
 
-        /**
-         *  нужен Метод по вызову волонтёра, нужна доработка
-         */
+        if (collectVolunteerUrl() != null) {
+            markup.addRow(new InlineKeyboardButton("Перейти к чату").url(collectVolunteerUrl()));
+            message = "Нажмите кнопку 'Перейти к чату' чтобы связатся ч волонтером";
+        }
+
 
         String message = "Уже позвал волонтёра. Скоро будет";
-        telegramBotSender.sendMessage(message, getChatId(update), telegramBotSender.setButtons(map));
+        telegramBotSender.sendMessage(message, getChatId(update), markup);
+    }
+
+    private String collectVolunteerUrl() {
+        User user = userService.findVolunteerByRole(VOLUNTEER);
+        if (user == null) {
+            message = "Извините я не смог найти волонтера";
+            return null;
+        }
+        return "http://t.me/" + user.getUserName();
     }
 }
